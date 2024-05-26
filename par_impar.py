069dd0dc9f043cee2c7bae4e8ad2cd44ca833c08
DBSSR <danielabassante@hotmail.com>
2024-05-26 15:52:29 -0500

    par o impar

---
+++ par_impar.py
@@ -0,0 +1,13 @@
+x = 10
+if x % 2 == 0:
+        print(f'{x} is an even number')
+else:
+        print(f'{x} is an odd number')
+
+#Quiero saber si hace push
+
+x = 13
+if x % 2 == 0:
+        print(f'{x} is an even number')
+else:
+        print(f'{x} is an odd number')
